6766d06ed8aa1ce4f694e85042b98e3130d21e60
Ankit singh <ankitsinghbca100@gmail.com>
2026-04-29 14:23:23 +0530

    first clean upload

---
+++ variables.py
@@ -0,0 +1,10 @@
+# Variables
+name = "Ankit" # here name is a variable
+age = 20 # age is a variable 
+price = 99.5 
+
+print(name, age, price)
+
+print("test upload")
+print("upload test")
+print("final upload working")
