ade3a6add33da0933541a994b8a845d20685d507
Ankit singh <ankitsinghbca100@gmail.com>
2026-05-01 17:47:59 +0530

    INPUT AND OUTPUT

---
+++ input_output.py
@@ -0,0 +1,22 @@
+# ============================
+# INPUT AND OUTPUT PRACTICE
+# ============================
+
+# taking  input from the user 
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Displaying output
+print("\n--- Output ---")
+print("Hello", name)
+print("Your age is ", age)
+
+# Eaxmple : Simple Addition by taking input
+
+num1 = int(input("\nEnter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum_result = num1 + num2
+
+print("Sum of numbers is: ", sum_result)
+
